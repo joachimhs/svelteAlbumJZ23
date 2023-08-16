c12979953652110343997bef11187c8aae0e12e7
Joachim Haagen Skeie <joachim@skeiene.no>
2023-08-16 17:43:04 +0200

    First attempt at gh-pages

--- src/routes/album/[albumid]/+layout.js
+++
@@ -1,6 +0,0 @@
-export async function load({parent, params}) {
-    const data = await parent();
-    let album = await data.albums.find((album) => album.caption === params.albumid);
-
-    return { album: album};
-}
--- src/routes/album/[albumid]/photo/[photoid]/+page.js
+++
@@ -1,7 +0,0 @@
-export async function load({parent, params}) {
-    const data = await parent();
-    let album = await data.albums.find((album) => album.caption === params.albumid);
-    let photo = await data.photos.find((photo) => photo.id = params.photoid);
-
-    return { album: album, photo: photo};
-}
